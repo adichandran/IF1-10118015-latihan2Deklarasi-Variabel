b57395870d83982368ab7da0cd6ae8a2590d9ed7
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 09:57:55 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan2deklarasivariabel/IF1101108015Latihan2Deklarasivariabel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan2deklarasivariabel;
+
+/**
+ *
+ * @author
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program ini berisi program mendeklarasikan variabel
+ */
+public class IF1101108015Latihan2Deklarasivariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        // Deklarasi variabel
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+        
+        //Memberi nilai variabel
+        nilaiInt      = 90;
+        nilaiLogika   = false;
+        nilaiKarakter = 'A';
+        
+        //Menampilkan hasil
+        System.out.println();
+        System.out.println("Isi variabel nilai\t= " + nilaiInt);
+        System.out.println("Isi variabel PHI\t= " + PHI);
+        System.out.println("Isi variabel logika\t= " + nilaiLogika);
+        System.out.println("Isi variabel Karakter\t= " + nilaiKarakter);
+    }
+    
+}
